bf95d58bba1c525c14f35537ef2b59fba6471c69
Mawci <79284679+Mawci@users.noreply.github.com>
2024-09-11 15:05:52 -0500

    TwentyOne Project

---
+++ TwentyOne/TwentyOne/Card.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class Card
+    {
+        //the card class has a property of datatype string called Suit
+        public string Suit {  get; set; }
+        public string Face { get; set; }
+    }
+}
