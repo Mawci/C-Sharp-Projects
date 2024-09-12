ac450dc7c339d210ba617fee7b47282cec89351c
Mawci <79284679+Mawci@users.noreply.github.com>
2024-09-11 19:33:21 -0500

    Calling Methods Submission Assignment

---
+++ CallingMethodsSubmission/CallingMethodsSubmission/MyClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsSubmission
+{
+    public class MyClass
+    {
+        public int addFive(int num)
+        {
+            return num += 5;
+        }
+
+        public int doubleNum(int num)
+        {
+            return num *= 2;
+        }
+
+        public int incrementNum(int num)
+        {
+            return ++num;
+        }
+    }
+}
---
+++ CallingMethodsSubmission/CallingMethodsSubmission/Program.cs
@@ -0,0 +1,34 @@
+﻿//===========================================================================================================
+//  Nick Rau | 9/11/2024 | Assignment: Calling Methods Submission
+//
+//
+//===========================================================================================================
+using System;
+using System.Collections.Generic;
+
+
+namespace CallingMethodsSubmission
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass tempClass = new MyClass();
+            int userInput;
+
+            Console.WriteLine("What number would you like to alter?");
+            userInput = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Calling the addFive function...");
+            Console.WriteLine(tempClass.addFive(userInput));
+
+            Console.WriteLine("Calling the doubleNum function...");
+            Console.WriteLine(tempClass.doubleNum(userInput));
+
+            Console.WriteLine("Calling the IncrementNum function...");
+            Console.WriteLine(tempClass.incrementNum(userInput));
+
+            Console.ReadLine();
+        }
+    }
+}
