a44600c7e77fc0b7a042a818dc368cd8743d94c3
Mawci <79284679+Mawci@users.noreply.github.com>
2024-09-12 08:53:18 -0500

    Method Class Submission Assignment

---
+++ MethodClassSubmission/MethodClassSubmission/Numbers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubmission
+{
+    internal class Numbers
+    {
+        public void mathNumbers(int num1, int num2)
+        {
+            num1 = num1 + 10;
+            Console.WriteLine(num2 + " is the second number that was passed to the function.");
+        }
+    }
+}
---
+++ MethodClassSubmission/MethodClassSubmission/Program.cs
@@ -0,0 +1,23 @@
+﻿//===========================================================================================================
+//  Nick Rau | 9/12/2024 | Assignment: Method Class Submission
+//
+//
+//===========================================================================================================
+using System;
+using System.Collections.Generic;
+
+
+namespace MethodClassSubmission
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers myClass = new Numbers();
+
+            myClass.mathNumbers(15, 343);
+            myClass.mathNumbers(num1: 20, num2: 777);
+            Console.ReadLine();
+        }
+    }
+}
