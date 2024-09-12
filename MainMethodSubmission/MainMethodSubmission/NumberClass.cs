d4876c986a4c3199483ef9dfcdc682b2cd2e355c
Mawci <79284679+Mawci@users.noreply.github.com>
2024-09-12 08:27:27 -0500

    Main Method Submission Assignment

---
+++ MainMethodSubmission/MainMethodSubmission/NumberClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmission
+{
+    public class NumberClass
+    {
+        public int mathOperation(int number)
+        {
+            number = number * number;
+            return number;
+        }
+
+        public int mathOperation(decimal number)
+        {
+            number = number * 2;
+            return (int)number;
+        }
+
+        public int mathOperation(string number)
+        {
+            return Convert.ToInt32(number) + 5;
+        }
+    }
+}
---
+++ MainMethodSubmission/MainMethodSubmission/Program.cs
@@ -0,0 +1,24 @@
+﻿//===========================================================================================================
+//  Nick Rau | 9/12/2024 | Assignment: Main Method Submission
+//
+//
+//===========================================================================================================
+using System;
+using System.Collections.Generic;
+
+
+namespace MainMethodSubmission
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberClass myClass = new NumberClass();
+
+            Console.WriteLine(myClass.mathOperation(75));
+            Console.WriteLine(myClass.mathOperation(75.098673621m));
+            Console.WriteLine(myClass.mathOperation("300"));
+            Console.ReadLine();
+        }
+    }
+}
